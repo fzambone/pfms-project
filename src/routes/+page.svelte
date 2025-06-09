5c8b4267069a83a592f264d874d5aea0ba89e958
Fernando Zambone <fernando.zambone@gmail.com>
2025-06-08 20:57:51 -0500

    Infra: set-up dependencies for bootstrap and prisma orm and create first migration for users and sessions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-center my-5">Welcome to your Personal Financial Management System!</h1>
+
+<div class="container text-center">
+  <button type="button" class="btn btn-primary btn-lg">
+    Get Started
+  </button>
+  <p class="mt-3">Bootstrap is working!</p>
+</div>
